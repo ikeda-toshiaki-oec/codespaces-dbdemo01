aa7e53b405d92aa03b473fade82c3034e98a563c
ikeda-toshiaki-oec <ikeda_toshiaki@edu.kimura.ac.jp>
2025-03-19 07:59:55 +0000

    250319 springboot3

--- db_demo/src/main/java/com/example/db_demo/DbDemoApplication.java
+++ db_demo/src/main/java/com/example/db_demo/DbDemoApplication.java
@@ -2,18 +2,9 @@ package com.example.db_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DbDemoApplication {
-
-	@RequestMapping("/")
-	String home(){
-		return "This is my APP!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DbDemoApplication.class, args);
 	}
